60a91c556442dd6656fed4e03d3263e56554d9de
Mike Taylor <20064376@mail.wit.ie>
2017-05-23 19:21:18 +0100

    Initial Commit

---
+++ For me to doTests/For_me_to_doTests.swift
@@ -0,0 +1,36 @@
+//
+//  For_me_to_doTests.swift
+//  For me to doTests
+//
+//  Created by Mike Taylor on 23/05/2017.
+//  Copyright © 2017 Mike Taylor. All rights reserved.
+//
+
+import XCTest
+@testable import For_me_to_do
+
+class For_me_to_doTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
