22e975ba96f761924a3b8db21c77ec1d9aa22332
Mike Taylor <20064376@mail.wit.ie>
2017-05-23 20:10:36 +0100

    added in haptic feedback

--- For me to do/ViewController.swift
+++ For me to do/ViewController.swift
@@ -12,6 +12,23 @@ class ViewController: UITableViewController {
     
     private var todoItems = ToDoItem.getMockData()
     
+    private func vibrateWithHaptic() {
+    let generator = UIImpactFeedbackGenerator(style: .heavy)
+    generator.prepare()
+    
+    generator.impactOccurred()
+    }
+    
+    func platform() -> String {
+        var sysinfo = utsname()
+        uname(&sysinfo) // ignore return value
+        return String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)!.trimmingCharacters(in: .controlCharacters)
+    }
+    
+    public var hasHapticFeedback: Bool {
+        return ["iPhone9,1", "iPhone9,3", "iPhone9,2", "iPhone9,4"].contains(platform())
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int
     {
         return 1
@@ -51,10 +68,66 @@ class ViewController: UITableViewController {
             tableView.reloadRows(at: [indexPath], with: .automatic)
         }
     }
+    
+    func didTapAddItemButton(_ sender: UIBarButtonItem)
+    {
+        // Create an alert
+        let alert = UIAlertController(
+            title: "New to-do item",
+            message: "Insert the title of the new to-do item:",
+            preferredStyle: .alert)
+        
+        // Add a text field to the alert for the new item's title
+        alert.addTextField(configurationHandler: nil)
+        
+        // Add a "cancel" button to the alert. This one doesn't need a handler
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        // Add a "OK" button to the alert. The handler calls addNewToDoItem()
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            if let title = alert.textFields?[0].text
+            {
+                self.addNewToDoItem(title: title)
+            }
+        }))
+        
+        // Present the alert to the user
+        self.present(alert, animated: true, completion: nil)
+        
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.impactOccurred()
+
+    }
+    
+    private func addNewToDoItem(title: String)
+    {
+        // The index of the new item will be the current item count
+        let newIndex = todoItems.count
+        
+        // Create new item and add it to the todo items list
+        todoItems.append(ToDoItem(title: title))
+        
+        // Tell the table view a new row has been created
+        tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .top)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if indexPath.row < todoItems.count
+        {
+            todoItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
+        }
+        
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+          self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.didTapAddItemButton(_:)))
+        
     }
 
     override func didReceiveMemoryWarning() {
