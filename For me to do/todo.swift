99a5ac5ea5662b03fe7567f2cd27dd9b5f1ed6ea
Mike Taylor <20064376@mail.wit.ie>
2017-05-24 10:35:45 +0100

    added in persistence

--- For me to do/todo.swift
+++ For me to do/todo.swift
@@ -6,9 +6,10 @@
 //  Copyright © 2017 Mike Taylor. All rights reserved.
 //
 
+
 import Foundation
 
-class ToDoItem
+class ToDoItem: NSObject, NSCoding
 {
     var title: String
     var done: Bool
@@ -18,6 +19,39 @@ class ToDoItem
         self.title = title
         self.done = false
     }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        // Try to unserialize the "title" variable
+        if let title = aDecoder.decodeObject(forKey: "title") as? String
+        {
+            self.title = title
+        }
+        else
+        {
+            // There were no objects encoded with the key "title",
+            // so that's an error.
+            return nil
+        }
+        
+        // Check if the key "done" exists, since decodeBool() always succeeds
+        if aDecoder.containsValue(forKey: "done")
+        {
+            self.done = aDecoder.decodeBool(forKey: "done")
+        }
+        else
+        {
+            // Same problem as above
+            return nil
+        }
+    }
+    
+    func encode(with aCoder: NSCoder)
+    {
+        // Store the objects into the coder object
+        aCoder.encode(self.title, forKey: "title")
+        aCoder.encode(self.done, forKey: "done")
+    }
 }
 
 extension ToDoItem
@@ -26,9 +60,61 @@ extension ToDoItem
     {
         return [
             ToDoItem(title: "Milk"),
-            ToDoItem(title: "Chocolate"),
-            ToDoItem(title: "Light bulb"),
-            ToDoItem(title: "Dog food")
+            ToDoItem(title: "Bread"),
+            ToDoItem(title: "Coffee"),
+            ToDoItem(title: "Gluten Free Rice Cakes")
         ]
+    }
+}
+
+// Creates an extension of the Collection type (aka an Array),
+// but only if it is an array of ToDoItem objects.
+extension Collection where Iterator.Element == ToDoItem
+{
+    // Builds the persistence URL. This is a location inside
+    // the "Application Support" directory for the App.
+    private static func persistencePath() -> URL?
+    {
+        let url = try? FileManager.default.url(
+            for: .applicationSupportDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: true)
+        
+        return url?.appendingPathComponent("todoitems.bin")
+    }
+    
+    // Write the array to persistence
+    func writeToPersistence() throws
+    {
+        if let url = Self.persistencePath(), let array = self as? NSArray
+        {
+            let data = NSKeyedArchiver.archivedData(withRootObject: array)
+            try data.write(to: url)
+        }
+        else
+        {
+            throw NSError(domain: "com.example.MyToDo", code: 10, userInfo: nil)
+        }
+    }
+    
+    // Read the array from persistence
+    static func readFromPersistence() throws -> [ToDoItem]
+    {
+        if let url = persistencePath(), let data = (try Data(contentsOf: url) as Data?)
+        {
+            if let array = NSKeyedUnarchiver.unarchiveObject(with: data) as? [ToDoItem]
+            {
+                return array
+            }
+            else
+            {
+                throw NSError(domain: "com.example.MyToDo", code: 11, userInfo: nil)
+            }
+        }
+        else
+        {
+            throw NSError(domain: "com.example.MyToDo", code: 12, userInfo: nil)
+        }
     }
 }
